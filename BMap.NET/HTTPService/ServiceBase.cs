bfbc4e10e84d9c7e35cc47a4e7ddf8519d6810db
李倍存 <libeicun@foxmail.com>
2017-10-27 16:23:09 +0800

    ! 修复代理设置导致的超时错误

--- BMap.NET/HTTPService/ServiceBase.cs
+++ BMap.NET/HTTPService/ServiceBase.cs
@@ -7,7 +7,7 @@ using System.Net;
 namespace BMap.NET.HTTPService
 {
     class WebClientWithTimeout : WebClient {
-        private int _timeout = 1000;
+        private int _timeout = 5000;
         public int Timeout {
             set {
                 _timeout = value;
@@ -34,6 +34,16 @@ namespace BMap.NET.HTTPService
         protected static string _ak = Properties.BMap.Default.ServiceAK;  //AK
         protected static string _sk = Properties.BMap.Default.ServiceSK;  //SK
         protected static VerificationMode _vm = (VerificationMode)Properties.BMap.Default.VerificationMode;  //校验方式 0表示IP白名单校验（忽略SK）   1表示SN校验（需要SK）
+
+        private WebClient wc;
+
+        public ServiceBase() {
+            wc = new WebClientWithTimeout();
+            wc.Encoding = Encoding.UTF8;
+            wc.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+            wc.Proxy = null;
+        }
+
         /// <summary>
         /// 从服务器上下载字符串
         /// </summary>
@@ -43,10 +53,7 @@ namespace BMap.NET.HTTPService
         {
             try
             {
-                WebClient client = new WebClientWithTimeout();
-                client.Encoding = Encoding.UTF8;
-                string str = client.DownloadString(url);
-                client.Dispose();
+                string str = wc.DownloadString(url);
                 return str;
             }
             catch
@@ -63,10 +70,7 @@ namespace BMap.NET.HTTPService
         {
             try
             {
-                WebClient client = new WebClientWithTimeout();
-                client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
-                byte[] data = client.DownloadData(url);
-                client.Dispose();
+                byte[] data = wc.DownloadData(url);
                 return data;
             }
             catch
