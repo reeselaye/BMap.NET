2968345880b759224a653d49cc584689b506c9b8
李倍存 <libeicun@foxmail.com>
2017-11-04 12:12:52 +0800

    + 提取经纬度-距离转换 Util

--- BMap.NET.WindowsForm/BMapControl.cs
+++ BMap.NET.WindowsForm/BMapControl.cs
@@ -2034,11 +2034,6 @@ namespace BMap.NET.WindowsForm
     /// </summary>
     class MapHelper
     {
-        public const double EARTH_RADIUS = 6378.137;//地球半径
-        private static double rad(double d)
-        {
-            return d * Math.PI / 180.0;
-        }
         /// <summary>
         /// 根据经纬度计算两点实际距离（m）
         /// </summary>
@@ -2047,15 +2042,7 @@ namespace BMap.NET.WindowsForm
         /// <returns></returns>
         public static double GetDistanceByLatLng(LatLngPoint p1, LatLngPoint p2)
         {
-            double radLat1 = rad(p1.Lat);
-            double radLat2 = rad(p2.Lat);
-            double a = radLat1 - radLat2;
-            double b = rad(p1.Lng) - rad(p2.Lng);
-            double s = 2 * Math.Asin(Math.Sqrt(Math.Pow(Math.Sin(a / 2), 2) +
-             Math.Cos(radLat1) * Math.Cos(radLat2) * Math.Pow(Math.Sin(b / 2), 2)));
-            s = s * EARTH_RADIUS;
-            s = Math.Round(s * 10000) / 10000;
-            return s;
+            return LatLngUtils.GetDistanceByLatLng(p1, p2);
         }
         /// <summary>
         /// 根据经纬度坐标计算该点在屏幕中的坐标
---
+++ BMap.NET.WindowsForm/LatLngUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BMap.NET.WindowsForm {
+    public class LatLngUtils {
+        public const double EARTH_RADIUS = 6378.137;//地球半径
+        private static double rad(double d) {
+            return d * Math.PI / 180.0;
+        }
+
+        public static double GetDistanceByLatLng(LatLngPoint p1, LatLngPoint p2) {
+            double radLat1 = rad(p1.Lat);
+            double radLat2 = rad(p2.Lat);
+            double a = radLat1 - radLat2;
+            double b = rad(p1.Lng) - rad(p2.Lng);
+            double s = 2 * Math.Asin(Math.Sqrt(Math.Pow(Math.Sin(a / 2), 2) +
+             Math.Cos(radLat1) * Math.Cos(radLat2) * Math.Pow(Math.Sin(b / 2), 2)));
+            s = s * EARTH_RADIUS;
+            s = Math.Round(s * 10000) / 10000;
+            return s;
+        }
+    }
+}
